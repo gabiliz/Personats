ad5b521e8ddfb92041ede5ae1628478a6ad1ff86
Gabriela-Liz <Gabriela-Liz@DESKTOP-S76PN19>
2021-05-25 19:51:08 -0300

    Add project files.

---
+++ personats/Data/Context/Context.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using personats.Data.Entity;
+
+namespace personats.Data.Context
+{
+    public class Context : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+    }
+}
---
+++ personats/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="personats.MvcApplication" Language="C#" %>
